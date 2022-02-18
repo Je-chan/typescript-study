0970fdcb47fdfa0f1e1455264451ed7241824de6
박예찬 <jkyyc3@gmail.com>
2022-02-18 23:53:34 +0900

    CREATE : Boolean Type

---
+++ basic/boolean.ts
@@ -0,0 +1,11 @@
+let isDone: boolean = false
+
+isDone = true;
+
+console.log(typeof isDone)
+
+let isOk: boolean = true
+// 아래 코드는 빨간 색 밑줄이 그려진다. 
+// let isNotOk: boolean = new Boolean(true)
+
+// 보통의 경우엔 직접 literal 값으로 primitive 타입을 적용한다.
