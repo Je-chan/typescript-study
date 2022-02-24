e0af8be231ef4c1c0cc9b66146789f732edbe3de
박예찬 <jkyyc3@gmail.com>
2022-02-24 23:01:58 +0900

    CREATE : Class Interface

---
+++ interface/classInterface.ts
@@ -0,0 +1,21 @@
+interface IPerson1 {
+  name: string;
+  age?: number;
+  hello(): void;
+}
+
+class Person implements IPerson1 {
+  name: string;
+  age?: number | undefined;
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  hello(): void {
+    console.log(`${this.name} 입니다`)
+  }  
+}
+
+const personClass: IPerson1 = new Person('Je')
+personClass.hello()
