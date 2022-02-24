cf11a1729e361b1d9bba6a6edfb34cc0be3915d3
박예찬 <jkyyc3@gmail.com>
2022-02-24 22:24:51 +0900

    CREATE : Function In Interface

--- interface/functionInterface.ts
+++ interface/functionInterface.ts
@@ -3,3 +3,32 @@ interface Person4 {
   age: number;
   hello(): void;
 }
+
+const p4_1: Person4 = {
+  name: 'Je',
+  age: 12,
+  hello: function() : void {
+    console.log(`Hello~ I'm ${this.name}`)
+  }
+}
+
+const p4_2: Person4 = {
+  name: 'Je',
+  age: 12,
+  hello() : void {
+    console.log(`Hello~ I'm ${this.name}`)
+  }
+}
+
+
+const p4_3: Person4 = {
+  name: 'Je',
+  age: 12,
+  hello: () : void => {
+    // arrow function 인 경우에는 this 를 사용할 수 없다.
+    // console.log(`Hello~ I'm ${this.name}`)
+  }
+}
+
+p4_1.hello()
+p4_2.hello()
